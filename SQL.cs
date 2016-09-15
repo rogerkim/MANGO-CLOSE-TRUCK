e22d43605f4aeb6cffb9b4b03bedac3440ac07ee
Jinsu Kim <dhammi@naver.com>
2016-09-16 08:52:27 +0900

    minor change

--- SQL.cs
+++ SQL.cs
@@ -8,7 +8,7 @@ namespace CloseTruck
 {
     public class SQL
     {
-        public static readonly string CONSTRING = "Server=VMKORWMSDBPD1;Database=LFLKR;User Id=itadmin;Password=ryan;";
+        public static readonly string CONSTRING = "****";
 
         public static readonly string SELECT_ORDER = "SELECT O.ExternOrderKey AS ID, O.C_Contact1 AS NAME, O.C_Zip AS ZIPCODE, O.C_Phone1 AS MOBILE, O.C_Address2 AS ADDRESS1, O.C_Address3 AS ADDRESS2, C.TrackingNumber AS TRACKINGNO, O.B_City AS TerminalCode FROM Orders O " +
                                                      "INNER JOIN CartonShipmentDetail C ON O.ExternOrderKey = C.ExternOrderKey " +
